f312aa99581c764b3dc817520700a4cfa346e477
susree3-2 <sumaiyasusree@gmail.com>
2026-04-21 01:50:32 +0600

    Connected login page to Django

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import index 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index), 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / "templates")
