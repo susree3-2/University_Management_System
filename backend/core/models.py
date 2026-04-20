15876bfac2d1656bd4e62f52ac22002676324754
susree3-2 <sumaiyasusree@gmail.com>
2026-04-21 03:44:25 +0600

    Showing data from database in student dashboard

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Course
 
 def index(request):
     return render(request, 'index.html')
 
 def student(request):
-    return render(request, 'student.html')
+    courses = Course.objects.all()   # get all courses
+    return render(request, 'student.html', {'courses': courses})
 
 def faculty(request):
     return render(request, 'faculty.html')
